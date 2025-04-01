6d4c90a9821ff9dbabb4b82b39c696927920626f
Thibout Sabine <124242397+sabine-thb@users.noreply.github.com>
2025-04-01 18:05:18 +0200

    levels transition & cubeMaps

--- Assets/Script/Score/ScoreManager.cs
+++ Assets/Script/Score/ScoreManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement; 
+using UnityEngine.Events; 
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TMP_Text scoreText; 
     private int score = 0;
+    public UnityEvent onWin;
 
     private void Awake()
     {
@@ -22,6 +25,9 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         scoreText.text = "Score : 0 / 10";
+
+         if (onWin == null)
+            onWin = new UnityEvent();
     }
 
     public void AddScore(int value)
@@ -29,6 +35,11 @@ public class ScoreManager : MonoBehaviour
         score += value;
         scoreText.text = "Score : " + score + " / 10";
         Debug.Log("AddPoint");
+
+         if (score >= 1)
+        {
+            LoadNextLevel();
+        }
     }
 
     public void ResetScore()
@@ -36,4 +47,38 @@ public class ScoreManager : MonoBehaviour
         score = 0;
         scoreText.text = "Score : " + score+ " / 10";
     }
+
+    private void LoadNextLevel()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // Déterminer la scène suivante
+        string nextSceneName = "";
+
+        if (currentSceneName == "Level 1")
+        {
+            nextSceneName = "Level 2";
+        }
+        else if (currentSceneName == "Level 2")
+        {
+            nextSceneName = "Level 3";
+        }
+        else if (currentSceneName == "Level 3")
+        {
+       
+            {
+                onWin.Invoke();
+                Debug.Log("Vous avez terminé tous les niveaux !");
+            }
+            return;
+        }
+        else
+        {
+            Debug.LogError("Nom de scène inconnu !");
+            return;
+        }
+
+            // Charger la scène suivante
+            SceneManager.LoadScene(nextSceneName);
+        }
 }
